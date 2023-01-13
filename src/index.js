2c24556c42102dc2e4c9e7be6e5c90eea4605eea
Uladzimir Bankouski <vlad.banek@gmail.com>
2023-01-13 20:19:46 +0300

    feat: added result

--- src/index.js
+++ src/index.js
@@ -1,3 +1,9 @@
 module.exports = function reverse (n) {
-  
+    // const result;
+    console.log(n);
+    let absN = Math.abs(n);
+    const stringRes = String(absN).split('').reverse().join('');
+    console.log(stringRes);
+    return +stringRes;
 }
+console.log(module.exports(162))
